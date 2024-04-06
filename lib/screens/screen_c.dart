f311ea5bb3da20d10dc93a3bd5b968f5372df320
Saddam Sinatrya <saddam.coder@gmail.com>
2024-04-06 09:19:58 +0700

    add message

--- lib/screens/screen_c.dart
+++ lib/screens/screen_c.dart
@@ -14,19 +14,24 @@ class ScreenC extends StatelessWidget {
             const Text('Screen C'),
             const SizedBox(height: 16),
             FilledButton(
-              onPressed: () => showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: const Text("Dialog"),
-                  content: const Text("This is a dialog"),
-                  actions: [
-                    TextButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: const Text("Close"),
-                    ),
-                  ],
-                ),
-              ),
+              onPressed: () async {
+                final result = await showDialog<String>(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text("Dialog"),
+                    content: const Text("This is a dialog"),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop("Dialog closed"),
+                        child: const Text("Close"),
+                      ),
+                    ],
+                  ),
+                );
+                if (result == null) return;
+                if (!context.mounted) return;
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(result)));
+              },
               child: const Text("Open Dialog"),
             ),
           ],
